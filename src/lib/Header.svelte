3d9fc863091fb00e50785a77fc182c298c33f4d1
sammy-code98 <sammysmart98@yahoo.com>
2021-10-15 08:30:08 +0100

    Feat:add header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import logo from "@/assets/svelte.png";
+</script>
+
+<header class="general-header">
+  <div class="logo">
+    <img src={logo} alt="app logo" class="logo-img" />
+    <div class="title">My Note App</div>
+  </div>
+</header>
+
+<style lang="scss">
+  .general-header {
+    background-color:rgb(214, 152, 162);
+    height: 56px;
+    color: white;
+    padding: 15px;
+    box-sizing: border-box;
+  }
+  .logo{
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+  .logo-img{
+    height: 100%;
+    margin-right: 15px;
+  }
+  .title{
+    font-weight: bold;
+  }
+</style>
